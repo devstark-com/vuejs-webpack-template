058bc9bbfbac965d5520562fcc9a82659c3f0c42
Saymon <saymon.biz@gmail.com>
2018-02-21 16:11:33 +0300

    debug

--- template/src/pages/_layout/LayoutBroker.vue
+++ template/src/pages/_layout/LayoutBroker.vue
@@ -34,13 +34,11 @@ export default {
     currentLayout () {
       return this.$route.meta.layout
     }
-  }{{if_and isAuth isVueProgress}},{{/if_and}}
+  },
   {{if_and isAuth isVueProgress}}
   created () {
     this.loadData()
   },
-  {{/if_and}}
-  {{#if_and isAuth isVueProgress}}
   methods: {
     loadData () {
       this.$Progress.start()
